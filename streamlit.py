28fcb6ad592697ba2b1e5a48add2b924c44e406b
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2024-09-08 23:48:46 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from frictionless import portals, Package
 
-package = Package("https://zenodo.org/record/7078760")
-report = catalog.packages[0].validate()
-st.write(report)
-
+if st.button("loading from zenodo"):
+  package = Package("https://zenodo.org/record/7078760")
+  report = catalog.packages[0].validate()
+  st.write(report)
+  st.write(catalog.packages[0].resources[0].to_pandas())
